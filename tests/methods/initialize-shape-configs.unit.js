59630328f7644b972e3bd555489398f7182f9883
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2021-02-24 08:57:18 -0500

    Added tests for `initializeShapeConfigs`

--- tests/methods/initialize-shape-configs.unit.js
+++ tests/methods/initialize-shape-configs.unit.js
@@ -1,16 +1,54 @@
 import initializeShapeConfigs from '../../src/methods/initialize-shape-configs';
 
 import ShapeConfig from '../../src/objects/shape-config';
+import { DEFAULT_SHAPE_OPTIONS, SHAPE_NAME } from '../../src/constants/static-properties';
 
 import random from '../shared/random';
 
 let instance, method;
 
-describe('#initializeFromOptions', () => {
+describe('#initializeShapeConfigs', () => {
   beforeEach(() => {
     createInstance();
 
     callMethod();
+  });
+
+  it('should set the expected values', () => {
+    const safeMarginsRatio = 0.2;
+
+    const expectedKeyValuePairs = {
+      safeMargins: instance.size * safeMarginsRatio,
+      safeSize: instance.size - instance.safeMargins,
+      equilateralTriangleWidth: instance.safeSize / 4,
+      equilateralTriangleHeight: (instance.equilateralTriangleWidth * Math.sqrt(3)) / 2,
+    };
+
+    expect(instance).toEqual(expect.objectContaining(expectedKeyValuePairs));
+  });
+
+  describe('attribute shapeConfigs', () => {
+    it('should contain all shape names associated ', () => {
+      const expectedShapeConfigs = {};
+      Object.values(SHAPE_NAME).forEach((shapeName) => {
+        expectedShapeConfigs[shapeName] = expect.any(ShapeConfig);
+      });
+
+      expect(instance.shapeConfigs).toEqual(expectedShapeConfigs);
+    });
+
+    it('should contain only valid paths', () => {
+      const validCoordinate = '[0-9]{1,3}(?:[.][0-9]{0,2})?';
+      const validCoordinates = `(?:${validCoordinate},${validCoordinate})`;
+      const validPathRegExp = new RegExp(`M${validCoordinates}(?:L${validCoordinates}){2,3}`);
+
+      let path;
+      Object.values(SHAPE_NAME).forEach((shapeName) => {
+        path = instance.shapeConfigs[shapeName].path;
+
+        expect(path).toMatch(validPathRegExp);
+      });
+    });
   });
 });
 
@@ -20,7 +58,10 @@ function callMethod(options = {}) {
 }
 
 function createInstance() {
-  instance = {};
+  instance = {
+    size: random.number({ min: 300, max: 800 }),
+    shapeOptions: DEFAULT_SHAPE_OPTIONS,
+  };
 
   method = initializeShapeConfigs.bind(instance);
 }
