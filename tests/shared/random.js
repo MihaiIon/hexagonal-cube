4359f59ad564b56329630feb9dc102c86597ec86
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2021-02-24 08:57:47 -0500

    Fixed `hexColor` helper

--- tests/shared/random.js
+++ tests/shared/random.js
@@ -4,12 +4,12 @@ const string = () => Math.random().toString(36).substring(7);
 export default {
   boolean: () => Math.random() < 0.5,
   elementFromArray,
-  hexColor: () => `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+  hexColor: () => `#${((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0')}`,
   keyFromObject: (obj = []) => elementFromArray(Object.keys(obj)),
   number: (options = {}) => {
-    const { max = Number.MAX_SAFE_INTEGER } = options;
+    const { min = 0, max = Number.MAX_SAFE_INTEGER } = options;
 
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * (max - min) + min);
   },
   string,
   stringArray: (count = 3) => new Array(count).fill(0).map(string),
